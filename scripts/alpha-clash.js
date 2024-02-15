079c904ad4117b2e3eb6caecd76184798a237f34
im-sanny <raselparvezsanny@gmail.com>
2024-02-15 13:00:44 +0600

    project alpha clash pro complete primarily

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -10,10 +10,80 @@
 //     // console.log(playgroundSection.classList)
 // }
 
+function handleKeyboardKeyUpEvent(event) {
+    const playerPressed = event.key;
+    console.log('player pressed', playerPressed);
+
+    // stop the game if pressed 'Esc'
+    if (playerPressed === 'Escape') {
+        gameOver();
+    }
+
+    // key player is expected to press
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLocaleLowerCase();
+    
+    // check right or wrong key pressed
+    if (playerPressed === expectedAlphabet) {
+        console.log('you get a point');
+
+        const currentScore = getTextElementValueById('current-score');
+        const updatedScore = currentScore + 1;
+        setTextElementValueById('current-score', updatedScore)
+       
+
+
+
+        //---------------------------------
+        // update score:
+        // 1. get the current score 
+        // const currentScoreElement = document.getElementById('current-score');
+        // const currentScoreText = currentScoreElement.innerText;
+        // const currentScore = parseInt(currentScoreText);
+        // console.log(currentScore);
+       
+
+        // // 2. increase the score by 1
+        const newScore = currentScore + 1;
+
+        // // 3. show the updated score 
+        // currentScoreElement.innerText = newScore;
+
+        // start a new round
+        removeBackgroundColorById(expectedAlphabet);
+        continueGame();
+    } else {
+        console.log('dude press the right key if ya wants point..');
+
+        const currentLife = getTextElementValueById('current-life');
+        const updatedLife = currentLife - 1;
+        setTextElementValueById('current-life', updatedLife);
+
+        if (updatedLife === 0) {
+            gameOver();
+            
+        }
+        // ------------------------------------
+        // step-1: get the current life number
+        // const currentLifeElement = document.getElementById('current-life');
+        // const currentLifeText = currentLifeElement.innerText;
+        // const currentLife = parseInt(currentLifeText);
+
+        // // step-2: reduce the count
+        // const newLife = currentLife - 1;
+
+        // // step-3: display the updated life count
+        // currentLifeElement.innerText = newLife;
+    }
+}
+document.addEventListener('keyup', handleKeyboardKeyUpEvent);
+
+// capture keyboard key press
 function continueGame() {
     // step-1: generate a random alphabet
     const alphabet = getARandomAlphabet();
-    console.log('your random alphabet', alphabet)
+    // console.log('your random alphabet', alphabet)
 
     // set randomly generated alphabet to the scree (show it)
     const currentAlphabetElement = document.getElementById('current-alphabet');
@@ -23,9 +93,29 @@ function continueGame() {
     setBackgroundColorById(alphabet);
 }
 function play() {
-    hideElementById('home-screen')
-    showElementById('play-ground')
-    continueGame()
+    // hide everything show only the playground
+    hideElementById('home-screen');
+    hideElementById('final-score');
+    showElementById('play-ground');
+
+    // reset score and life
+    setTextElementValueById('current-life', 5);
+    setTextElementValueById('current-score', 0);
+
+    continueGame();
 }
 
+function gameOver() {
+    hideElementById('play-ground');
+    showElementById('final-score');
+    //update final score
+    // 1. get the final score
+    const lastScore = getTextElementValueById('current-score');
+    console.log(lastScore);
+    setTextElementValueById('game-score', lastScore);
 
+    // clear the last selected alphabet
+    const currentAlphabet = getElementTextById('current-alphabet');
+    // console.log(currentAlphabet);
+    removeBackgroundColorById(currentAlphabet);
+}
